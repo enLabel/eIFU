bd100034fac41d812cbb0e52a51f4f7657f5828f
Sean Kerins <skerins@enlabel.com>
2016-04-11 16:16:52 -0400

    Added some files

---
+++ js/eifuMain.js
@@ -0,0 +1,17 @@
+function topMenuRotate() {
+			    // Code for Chrome, Safari, Opera
+			    document.getElementById("topMenu").style.WebkitTransform = "rotate(180deg)"; 
+			    // Code for IE9
+			    document.getElementById("topMenu").style.msTransform = "rotate(180deg)"; 
+			    document.getElementById("topMenu").style.transform = "rotate(180deg)"; 
+			}
+
+function menuRotate() {
+			    // Code for Chrome, Safari, Opera
+			    document.getElementById("botttomMenu").style.WebkitTransform = "rotate(180deg)"; 
+			    // Code for IE9
+			    document.getElementById("botttomMenu").style.msTransform = "rotate(180deg)"; 
+			    document.getElementById("botttomMenu").style.transform = "rotate(180deg)"; 
+			} 
+
+			botttomMenu
